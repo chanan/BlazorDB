59399e965bc36a75b2d64cb45b2bbdf49f5a2630
Chanan Braunstein <chanan@bkbunch.com>
2018-05-13 10:24:59 -0600

    Saving and loading one to one associations

--- src/BlazorDB/Storage/StorageManager.cs
+++ src/BlazorDB/Storage/StorageManager.cs
@@ -19,10 +19,8 @@ namespace BlazorDB.Storage
         {
             int total = 0;
             var contextType = context.GetType();
-            //Logger.ContextSaved(contextType);
-            var storageSets = (from prop in contextType.GetProperties()
-                            where prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(StorageSet<>)
-                            select prop).ToList();
+            Logger.ContextSaved(contextType);
+            var storageSets = GetStorageSets(contextType);
             foreach (var prop in storageSets)
             {
                 var storageSetValue = prop.GetValue(context);
@@ -35,7 +33,7 @@ namespace BlazorDB.Storage
                 if (oldMetadata != null) DeleteOldModelsFromStorage(oldMetadata, storageTableName);
                 Logger.StorageSetSaved(modelType, guids.Count);
             }
-            //Logger.EndGroup();
+            Logger.EndGroup();
             return total;
         }
 
@@ -43,21 +41,241 @@ namespace BlazorDB.Storage
         {
             var context = Activator.CreateInstance(contextType);
             Logger.StartContextType(contextType);
+            var storageSets = GetStorageSets(contextType);
+            var stringModels = LoadStringModels(contextType, storageSets);
+            //PrintStringModels(stringModels);
+            stringModels = ScanNonAssociationModels(storageSets, stringModels);
+            stringModels = ScanAssociationModels(storageSets, stringModels);
+            stringModels = DeserializeModels(stringModels);
             foreach (var prop in contextType.GetProperties())
             {
                 if (prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(StorageSet<>))
                 {
                     var modelType = prop.PropertyType.GetGenericArguments()[0];
-                    Logger.LoadModelInContext(modelType);
                     var storageSetType = genericStorageSetType.MakeGenericType(modelType);
                     var storageTableName = Util.GetStorageTableName(contextType, modelType);
                     var metadata = LoadMetadata(storageTableName);
-                    var storageSet = metadata != null ? LoadStorageSet(metadata, storageTableName, storageSetType, contextType, modelType) : CreateNewStorageSet(storageSetType);
+                    if(stringModels.ContainsKey(modelType))
+                    {
+                        var map = stringModels[modelType];
+                        Logger.LoadModelInContext(modelType, map.Count);
+                    }
+                    else
+                    {
+                        Logger.LoadModelInContext(modelType, 0);
+                    }
+                    var storageSet = metadata != null ? LoadStorageSet(metadata, storageTableName, storageSetType, contextType, modelType, stringModels[modelType]) : CreateNewStorageSet(storageSetType);
                     prop.SetValue(context, storageSet);
                 }
             }
             RegisterContext(serviceCollection, contextType, context);
             Logger.EndGroup();
+        }
+
+        private Dictionary<Type, Dictionary<int, SerializedModel>> DeserializeModels(Dictionary<Type, Dictionary<int, SerializedModel>> stringModels)
+        {
+            foreach (var map in stringModels)
+            {
+                Type modelType = map.Key;
+                foreach (var sm in map.Value)
+                {
+                    var stringModel = sm.Value;
+                    stringModel.Model = DeserializeModel(modelType, stringModel.StringModel);
+                }
+            }
+            return stringModels;
+        }
+
+        private object LoadStorageSet(Metadata metadata, string storageTableName, Type storageSetType, Type contextType, Type modelType, Dictionary<int, SerializedModel> map)
+        {
+            var instance = CreateNewStorageSet(storageSetType);
+            var prop = storageSetType.GetProperty("StorageContextTypeName", flags);
+            prop.SetValue(instance, Util.GetFullyQualifiedTypeName(contextType));
+            var listGenericType = genericListType.MakeGenericType(modelType);
+            var list = Activator.CreateInstance(listGenericType);
+            foreach (var sm in map)
+            {
+                var stringModel = sm.Value;
+                var addMethod = listGenericType.GetMethod("Add");
+                addMethod.Invoke(list, new object[] { stringModel.Model });
+            }
+            return SetList(instance, list);
+        }
+
+        private Dictionary<Type, Dictionary<int, SerializedModel>> ScanNonAssociationModels(List<PropertyInfo> storageSets, Dictionary<Type, Dictionary<int, SerializedModel>> stringModels)
+        {
+            foreach (var map in stringModels)
+            {
+                Type modelType = map.Key;
+                foreach (var sm in map.Value)
+                {
+                    var stringModel = sm.Value;
+                    if (!HasAssociation(storageSets, modelType, stringModel))
+                    {
+                        stringModel.HasAssociation = false;
+                        stringModel.ScanDone = true;
+                    }
+                    else
+                    {
+                        stringModel.HasAssociation = true;
+                    }
+                }
+            }
+            return stringModels;
+        }
+
+        private bool HasAssociation(List<PropertyInfo> storageSets, Type modelType, SerializedModel stringModel)
+        {
+            var found = false;
+            foreach (var prop in modelType.GetProperties())
+            {
+                if (IsInContext(storageSets, prop))
+                {
+                    found = true;
+                    break;
+                }
+            }
+            return found;
+        }
+
+        private Dictionary<Type, Dictionary<int, SerializedModel>> ScanAssociationModels(List<PropertyInfo> storageSets, Dictionary<Type, Dictionary<int, SerializedModel>> stringModels)
+        {
+            var count = 0;
+            do
+            {
+                count++;
+                foreach (var map in stringModels)
+                {
+                    Type modelType = map.Key;
+                    foreach (var sm in map.Value)
+                    {
+                        var stringModel = sm.Value;
+                        if (!stringModel.ScanDone)
+                        {
+                            if(HasAssociation(storageSets, modelType, stringModel))
+                            {
+                                stringModel.StringModel = FixAssociationsInStringModels(stringModel, modelType, storageSets, stringModels);
+                            }
+                            else
+                            {
+                                stringModel.ScanDone = true;
+                            }
+                        }
+                    }
+                }
+                if (count == 5) break;
+            } while (IsScanDone(stringModels));
+            return stringModels;
+        }
+
+        void PrintStringModels(Dictionary<Type, Dictionary<int, SerializedModel>> stringModels)
+        {
+            foreach (var map in stringModels)
+            {
+                Type modelType = map.Key;
+                Console.WriteLine("modelType: {0}", modelType.Name);
+                foreach (var sm in map.Value)
+                {
+                    var stringModel = sm.Value;
+                    Console.WriteLine("sm: {0}", sm.Value.StringModel);
+                    Console.WriteLine("Is Done: {0}", sm.Value.ScanDone);
+                }
+            }
+
+        }
+
+        private string FixAssociationsInStringModels(SerializedModel stringModel, Type modelType, List<PropertyInfo> storageSets, Dictionary<Type, Dictionary<int, SerializedModel>> stringModels)
+        {
+            var result = stringModel.StringModel;
+            foreach (var prop in modelType.GetProperties())
+            {
+                if (IsInContext(storageSets, prop))
+                {
+                    var Id = FindIdInSerializedModel(result);
+                    var updated = GetAssociatedStringModel(stringModels, prop.PropertyType, Id);
+                    result = ReplaceIdWithAssociation(result, prop.Name, Id, updated);
+                }
+            }
+            return result;
+        }
+
+        private string GetAssociatedStringModel(Dictionary<Type, Dictionary<int, SerializedModel>> stringModels, Type modelType, int id)
+        {
+            var map = stringModels[modelType];
+            return map[id].StringModel;
+        }
+
+        //TODO: Convert to Linq
+        private bool IsScanDone(Dictionary<Type, Dictionary<int, SerializedModel>> stringModels)
+        {
+            var done = true;
+            foreach(var map in stringModels.Values)
+            {
+                foreach(var sm in map.Values)
+                {
+                    if (!sm.ScanDone) done = false;
+                }
+            }
+            return done;
+        }
+
+        private Dictionary<Type, Dictionary<int, SerializedModel>> LoadStringModels(Type contextType, List<PropertyInfo> storageSets)
+        {
+            var stringModels = new Dictionary<Type, Dictionary<int, SerializedModel>>();
+            foreach (var prop in storageSets)
+            {
+                var modelType = prop.PropertyType.GetGenericArguments()[0];
+                var map = new Dictionary<int, SerializedModel>();
+                var storageTableName = Util.GetStorageTableName(contextType, modelType);
+                var metadata = LoadMetadata(storageTableName);
+                if(metadata != null)
+                {
+                    foreach (var guid in metadata.Guids)
+                    {
+                        var name = $"{storageTableName}-{guid}";
+                        var serializedModel = BlazorDBInterop.GetItem(name, false);
+                        var Id = FindIdInSerializedModel(serializedModel);
+                        map.Add(Id, new SerializedModel { StringModel = serializedModel });
+                    }
+                    stringModels.Add(modelType, map);
+                }
+            }
+            return stringModels;
+        }
+
+        //TODO: Verify that the found id is at the top level in case of nested objects
+        private int FindIdInSerializedModel(string serializedModel)
+        {
+            var start = serializedModel.IndexOf("\"Id\":");
+            return GetIdFromString(serializedModel, start);
+        }
+
+        private int GetIdFromString(string stringToSearch, int startFrom = 0)
+        {
+            var foundFirst = false;
+            var arr = stringToSearch.ToCharArray();
+            var result = new List<char>();
+            for (int i = startFrom; i < arr.Length; i++)
+            {
+                var ch = arr[i];
+                if (Char.IsDigit(ch))
+                {
+                    foundFirst = true;
+                    result.Add(ch);
+                }
+                else
+                {
+                    if (foundFirst) break;
+                }
+            }
+            return Convert.ToInt32(new string(result.ToArray()));
+        }
+
+        private List<PropertyInfo> GetStorageSets(Type contextType)
+        {
+            return (from prop in contextType.GetProperties()
+                    where prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(StorageSet<>)
+                    select prop).ToList();
         }
 
         private void DeleteOldModelsFromStorage(Metadata metadata, string storageTableName)
@@ -102,19 +320,17 @@ namespace BlazorDB.Storage
             {
                 if(IsInContext(storageSets, prop) && prop.GetValue(model) != null)
                 {
-                    Console.WriteLine("Found Association");
                     var associatedModel = prop.GetValue(model);
                     var IdProp = associatedModel.GetType().GetProperty("Id"); //TODO: Handle missing Id prop
-                    var id = IdProp.GetValue(associatedModel);
-                    Console.WriteLine("Found Association: {0}, Id: {1}", prop.Name, id);
-                    result = ReplaceAssociation(result, prop.Name, id);
+                    var id = Convert.ToString(IdProp.GetValue(associatedModel));
+                    result = ReplaceAssociationWithId(result, prop.Name, id);
                 }
             }
             return result;
         }
 
         //TODO: A better way to do this would be to use dynamic, which right now doesn't work in Blazor
-        private string ReplaceAssociation(string result, string name, object id)
+        private string ReplaceAssociationWithId(string result, string name, string id)
         {
             var stringToFind = $"\"{name}\":{{";
             var start = result.IndexOf(stringToFind);
@@ -140,11 +356,11 @@ namespace BlazorDB.Storage
             return result;
         }
 
-        private string ReplaceString(string result, int open, int close, object id)
+        private string ReplaceString(string source, int start, int end, string stringToInsert)
         {
-            var start = result.Substring(0, open);
-            var end = result.Substring(close);
-            return start + id + end;
+            var startStr = source.Substring(0, start);
+            var endStr = source.Substring(end);
+            return startStr + stringToInsert + endStr;
         }
 
         private bool IsInContext(List<PropertyInfo> storageSets, PropertyInfo prop)
@@ -174,22 +390,13 @@ namespace BlazorDB.Storage
             return Activator.CreateInstance(storageSetType);
         }
 
-        private object LoadStorageSet(Metadata metadata, string storageTableName, Type storageSetType, Type contextType, Type modelType)
+        private string ReplaceIdWithAssociation(string result, string name, int id, string stringModel)
         {
-            var instance = CreateNewStorageSet(storageSetType);
-            var prop = storageSetType.GetProperty("StorageContextTypeName", flags);
-            prop.SetValue(instance, Util.GetFullyQualifiedTypeName(contextType));
-            var listGenericType = genericListType.MakeGenericType(modelType);
-            var list = Activator.CreateInstance(listGenericType);
-            foreach (var guid in metadata.Guids)
-            {
-                var name = $"{storageTableName}-{guid}";
-                var value = BlazorDBInterop.GetItem(name, false);
-                var model = Deserialize(modelType, value);
-                var addMethod = listGenericType.GetMethod("Add");
-                addMethod.Invoke(list, new object[] { model });
-            }
-            return SetList(instance, list);
+            var stringToFind = $"\"{name}\":{id}";
+            var nameIndex = result.IndexOf(stringToFind);
+            var index = result.IndexOf(id.ToString(), nameIndex);
+            result = ReplaceString(result, index, index + id.ToString().Length, stringModel);
+            return result;
         }
 
         private object SetList(object instance, object list)
@@ -199,7 +406,7 @@ namespace BlazorDB.Storage
             return instance;
         }
 
-        private object Deserialize(Type modelType, string value)
+        private object DeserializeModel(Type modelType, string value)
         {
             var method = typeof(JsonWrapper).GetMethod("Deserialize");
             var genericMethod = method.MakeGenericMethod(modelType);
